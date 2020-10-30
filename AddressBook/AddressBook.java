a885f78c4cecf5ad30c7a28cbe342fea0e0e892d
Birajit <birajit95@gmail.com>
2020-10-30 10:38:10 +0530

    Add: Welcome message is displayed

---
+++ AddressBook/AddressBook.java
@@ -0,0 +1,16 @@
+import javax.swing.text.Style;
+
+import java.time.LocalTime; 
+public class AddressBook{
+    public static void welcomeMessage(){
+      System.out.println(" Welcome to Address Book--The time is "+LocalTime.now());
+    }
+    public static void main(String []args){
+      
+    welcomeMessage();
+
+}
+}
+
+
+
